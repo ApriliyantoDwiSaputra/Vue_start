f260ac960d0570f34a903d9049944eb1ad60e73a
apriliyanto dwi saputra <aprildwi999@gmail.com>
2025-09-01 14:09:29 +0700

    setting-ui

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,7 +1,69 @@
 <template>
     <main class="about-page">
-        <h1>About</h1>
-        <p>This is about Page</p>
+        <div class="judul">
+            <h2>System settings</h2>
+            <p>Please enable system alert you will get.</p>
+        </div>
+
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text">
+                        <span>Notify me about new features and updates</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div class="toggle">
+                        <input type="checkbox" checked>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="text">
+                        <span>Notify me by email for latest news</span>
+                        <p>Nulla et tincidunt sapien. Sed eleifend volutpat elementum.</p>
+                    </div>
+                    <div class="toggle">
+                        <input type="checkbox" checked>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="text">
+                        <span>Notify me about tips on using account</span>
+                        <p>Donec in quam sed urna bibendum tincidunt quis mollis mauris.</p>
+                    </div>
+                    <div class="toggle">
+                        <input type="checkbox" checked>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="judul">
+            <h2>Security settings</h2>
+            <p>Control security alert you will be notified.</p>
+        </div>
+
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text">
+                        <span>Unusual activity notifications</span>
+                        <p>Donec in quam sed urna bibendum tincidunt quis mollis mauris.</p>
+                    </div>
+                    <div class="toggle">
+                        <input type="checkbox" checked>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="text">
+                        <span>Unauthorized financial activity</span>
+                        <p>Fusce lacinia elementum eros, sed vulputate urna eleifend nec.</p>
+                    </div>
+                    <div class="toggle">
+                        <input type="checkbox" checked>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -10,5 +72,38 @@
 </script>
 
 <style lang="scss" scoped>
+main {
+    padding: 1.5rem 5rem;
+    color: var(--dark-alt);
+    
+    h2 {
+        font-size: 1rem;
+        font-weight: 600;
+    }
+}
 
+.container {
+    margin: 0.5rem 2rem;
+    width: 90%;
+    background-color: var(--dark-alt);
+    color: var(--light);
+    padding: 1rem;
+    border-radius: 6px;
+}
+
+.card {
+    width: 100%;
+}
+
+.card-body {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    border-bottom: 1px solid var(--dark);
+
+    &:last-child {
+        border-bottom: none;
+    }
+}
 </style>
